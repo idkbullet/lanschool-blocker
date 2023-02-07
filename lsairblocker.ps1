9b92fd0b3d78c883eaae56333733b4c10ac99fae
Bullet <101953487+idkbullet@users.noreply.github.com>
2023-02-07 10:16:24 -0500

    Add files via upload

---
+++ lsairblocker.ps1
@@ -0,0 +1,20 @@
+﻿#What's new 4/26/21
+#Pressing ESC safely closes the blocker
+
+
+
+Add-Type -AssemblyName WindowsBase
+Add-Type -AssemblyName PresentationCore
+
+Write-Output "Welcome to bullet's LanSchool Air Terminator V1.2! Opened at "(get-date).ToString('T');
+Write-Output "To abort at any time, press ESC."
+while($true){
+taskkill /im LSAirClient.exe /f
+taskkill /im LSAirClientUI.exe /f
+  $isDown = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::Escape)
+  if ($isDown)
+  {
+    Write-Output "Blocker Deactivated. Reopen it to resume blocking"
+    break
+  }
+  }
