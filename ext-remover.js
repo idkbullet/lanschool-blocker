4d002369cd02a25bd802bf28b20ef697ca898d5e
Bullet <101953487+idkbullet@users.noreply.github.com>
2023-04-17 14:20:21 -0400

    Update and rename ext-remover.js to chrome-version.js

--- ext-remover.js
+++
@@ -1,1 +0,0 @@
-javascript:let shim = false;var ids = prompt("extension ids (comma separated)").split(",");setInterval(()=>{ids.forEach((id)=> opener.chrome.developerPrivate.updateExtensionConfiguration({extensionId: id, fileAccess: shim}));shim = !shim;}, 145);
