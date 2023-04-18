97de63c765c4bff719c641a73d096d9c08c0642f
Bullet <101953487+idkbullet@users.noreply.github.com>
2023-04-18 10:18:18 -0400

    Delete edge-version.js

--- edge-version.js
+++
@@ -1,1 +0,0 @@
-let shim = false;var ids = prompt("extension ids (comma separated)").split(",");setInterval(()=>{ids.forEach((id)=> opener.edge.developerPrivate.updateExtensionConfiguration({extensionId: id, fileAccess: shim}));shim = !shim;}, 145);
